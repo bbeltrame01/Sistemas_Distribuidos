35d79b4dd0a883b2eb5b81c269a7aede5b6db93c
Crixin <47997631+Crixin@users.noreply.github.com>
2019-11-19 19:25:45 -0300

    Update Server.py

--- Trab_Final/Server.py
+++ Trab_Final/Server.py
@@ -2,7 +2,12 @@
 """Server for multithreaded (asynchronous) chat application."""
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
+import collections
+import math
 
+respClients = []
+totalClients = 3
+operacao = ""
 def accept_incoming_connections():
     """Sets up handling for incoming clients."""
     while True:
@@ -11,9 +16,9 @@ def accept_incoming_connections():
         print("%s has connected." % client_address[0])
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,client_address[0],)).start()
-        if len(addresses) == 2:
-            operacao = input("Entre com a operação ( 1 = ligar/ 2 = desligar)")
-
+        if len(addresses) == totalClients:
+            entrada = input("Entre com a operação ( 1 = ligar/ 2 = desligar)")
+            operacao = entrada
             mandaMsg();
 
 
@@ -23,16 +28,33 @@ def handle_client(client, name):  # Takes client socket as argument.
     clients[client] = client
 
     while True:
-        global bTrue
         msg = client.recv(BUFSIZ)
         if msg:
+            respClients.append(msg)
             print(name, msg)
+            if len(respClients) == totalClients:
+                consenso()
 
+
+def consenso():
+    respostas = collections.Counter(respClients)
+    result = False
+    for item in respostas.most_common():
+        # 0 valor
+        # 1 qtd
+
+        if (math.trunc(len(respClients) / 3) + 1) < item[1]:
+            result = item[0].decode("utf-8")
+
+    if bool(result) and result != "False":
+        print("A operação será executada")
+    else:
+        print("Sem consenso, nenhuma operação será executada")
 
 def mandaMsg():  # prefix is for name identification.
     """Broadcasts a message to all the clients."""
     for sock in clients:
-        ip = (sock.getsockname()[0])
+        ip = (sock.getpeername()[0])
         msg = input("Entre com o valor para o "+ ip+" ")
         sock.send(bytes("Servidor:" +msg, "utf8"))
 
